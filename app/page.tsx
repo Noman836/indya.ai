dd8c4bd54e8ebb24a7d1b9ee393d5cc8ec5bcc4b
nomanrao1 <nomanamirbros@gmail.com>
2024-09-15 23:14:58 -0700

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -25,8 +25,8 @@ export default function Home() {
           <h3 className="border border-boxBorderColor text-secondary bg-boxbgColor text-center px-[20px] py-[8px] rounded-[20px]">Transform Cloud Security with Cleo’s Advanced AI</h3>
         </div>
         <div className="w-2/4 py-[24px]">
-          <h2 className="text-center text-[50px] font-[700] leading-[64px]">
-            AI-Driven Cloud Security,  Reimagined
+          <h2 className="text-center text-[50px] font-[700] leading-[64px] text-primary">
+            AI-Driven Cloud Security, <br /> Reimagined
           </h2>
         </div>
         <div className="">
@@ -38,10 +38,10 @@ export default function Home() {
         <div className="xl:pt-[100px]  w-full flex justify-center relative curve-image">
 
           {/* <BorderBeam /> */}
-          <div className="w-3/4 p-10 border border-boxBorderColor rounded-[10px] flex flex-col items-center relative top-[100px] gap-5 text-black bg-red-400">
+          <div className="w-3/4 p-10 border border-boxBorderColor rounded-[10px] flex flex-col items-center relative top-[100px] gap-5 text-black">
             <Button className="text-[14px] font-[400] text-primary bg-boxbgColor border border-boxBorderColor px-[20px] py-[6px] rounded-[30px]">Feature</Button>
-            <h3 className="w-2/3 text-center text-[40px] font-[700] leading-[50px]">Comprehensive Cloud Security Management Solutions</h3>
-            <h2 className="w-2/3 text-center font-[400] leading-[24px] text-[24px]">Real-time threat detection, automated compliance checks, and robust data encryption for your cloud environment.</h2>
+            <h3 className="w-2/3 text-center text-[40px] font-[700] leading-[50px] text-primary">Comprehensive Cloud Security Management Solutions</h3>
+            <h2 className="w-2/3 text-center font-[400] leading-[24px] text-[24px] text-primary">Real-time threat detection, automated compliance checks, and robust data encryption for your cloud environment.</h2>
             <Image
               src={Group}
               alt="Contact Us Image"
